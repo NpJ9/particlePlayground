95206acad161ea2c2d17d7f722ed3b88b0323481
Jack Naylor <jacknaylor999@hotmail.com>
2025-03-06 12:04:23 +0700

    added web audio api

--- main.js
+++ main.js
@@ -12,15 +12,26 @@ window.addEventListener("load", () =>{
     gameLoop();
 });
 
-function generateParticles(){
+window.addEventListener("click", (e) => { // Onclick generate particles
     for(let i = 0; i < numParticles; i++){
+        let mouseX = e.offsetX;
+        let mouseY = e.offsetY;
+        let randomSize = Math.random() * 20 + 5;
+        let randomColour = coloursArray[Math.floor(Math.random() * coloursArray.length)]
+        particles.push(new Particle(mouseX, mouseY, randomSize, 5, randomColour));
+    };
+})
+
+function generateParticles(){
+        for(let i = 0; i < numParticles; i++){
         let randomX = canvas.width / 2;
         let randomY = canvas.height / 2;
         let randomSize = Math.random() * 20 + 5;
         let randomColour = coloursArray[Math.floor(Math.random() * coloursArray.length)]
         particles.push(new Particle(randomX, randomY, randomSize, 5, randomColour));
-    };
-};
+
+    }
+}
 
 function drawParticles(){  
     particles.forEach(particle => {
@@ -31,11 +42,13 @@ function drawParticles(){
             // Do something fun
             generateParticles(); // Create the void 
             ctx.clearRect(0, 0, canvas.width, canvas.height); 
+            playSound()
         }
         
         if(particle.y - particle.radius <= 0 || particle.y + particle.radius >= canvas.height){ // Check collisions for y axis
             // Do something fun
-            ctx.clearRect(0, 0, canvas.width, canvas.height)   
+            ctx.clearRect(0, 0, canvas.width, canvas.height); 
+            playSound()
         }
   });
 };
@@ -51,3 +64,38 @@ function resize(){
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
 };
+
+// Sets up Audio Enviroment 
+
+const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+let soundBuffer = null;
+
+fetch("sound.wav")
+.then(response => response.arrayBuffer())
+.then(arrayBuffer => audioContext.decodeAudioData(arrayBuffer))
+.then(decodedBuffer => {
+    soundBuffer = decodedBuffer
+})
+.catch(e => console.error(e));
+
+// Check Current Time vs Last Time Sound Was Played: Wait atleast 200ms
+let lastSoundTime = 0;
+
+function playSound(){
+    const now = Date.now();
+    if(now - lastSoundTime < 100) return;
+    lastSoundTime = now;
+
+    if (!soundBuffer) return; // Ensurer the sound is loaded
+
+    const source = audioContext.createBufferSource();
+    source.buffer = soundBuffer;
+
+    const gainNode = audioContext.createGain();
+    gainNode.gain.setValueAtTime(0.10, audioContext.currentTime); // STart at 0.05 for slight more attack
+    gainNode.gain.linearRampToValueAtTime(0.05, audioContext.currentTime + 0.05);// Increase Volumne to 0.2 in 50ms
+
+    source.connect(gainNode);
+    gainNode.connect(audioContext.destination);
+    source.start();
+}
